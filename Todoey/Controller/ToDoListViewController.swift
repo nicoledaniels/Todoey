792df2982ba2cb9de2c97d9ddd48411df4bbbb32
Nicole Daniels <nzoner12@yahoo.com>
2018-07-14 15:07:54 -0400

    MVC structure

--- Todoey/Controller/ToDoListViewController.swift
+++ Todoey/Controller/ToDoListViewController.swift
@@ -10,21 +10,23 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    var itemArray = ["Buy Milk", "Clean Room", "Fight the Patriarchy"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
-            itemArray = items
+        if let items = defaults.object(forKey: "ToDoListArray") as? NSData {
+            itemArray = (NSKeyedUnarchiver.unarchiveObject(with: items as Data) as? [Item])!
         }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoListItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.accessoryType = item.done ? .checkmark : .none
+        cell.textLabel?.text = itemArray[indexPath.row].title
         return cell
     }
     
@@ -35,13 +37,13 @@ class ToDoListViewController: UITableViewController {
     //MARK - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        let encodedData = NSKeyedArchiver.archivedData(withRootObject: self.itemArray)
+        self.defaults.set(encodedData, forKey: "ToDoListArray")
+        
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
-        let currentCell = tableView.cellForRow(at: indexPath)
-        if currentCell?.accessoryType == .checkmark {
-            currentCell?.accessoryType = .none
-        } else {
-            currentCell?.accessoryType = .checkmark
-        }
     }
     
     //MARK - Add New Items
@@ -50,8 +52,11 @@ class ToDoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New ToDoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen after add item button is clicked
-            self.itemArray.append(textField.text!)
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            let encodedData = NSKeyedArchiver.archivedData(withRootObject: self.itemArray)
+            self.defaults.set(encodedData, forKey: "ToDoListArray")
             self.tableView.reloadData()
         }
             alert.addTextField{ (alertTextField) in
